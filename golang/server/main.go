117c39326d1395131fe9b80a6d17653e91301835
Nik Swap <nikswap@local.host>
2023-12-25 12:36:07 +0100

    Updated server with initial struct format

--- golang/server/main.go
+++ golang/server/main.go
@@ -1,9 +1,22 @@
 package server
 
+import (
+	"fmt"
+	"time"
+)
+
 // This server should:
 // * Receive and handle connections from implant
-// * Server plugins to implants
+// * Serve plugins to implants
+
+type Implant struct {
+	HostName           string
+	LastKnownIp        string
+	LastKnownHeartBeat time.Time
+	PluginsToLoad      []string
+}
 
 func main() {
-
+	nu := time.Now()
+	fmt.Println(nu)
 }
